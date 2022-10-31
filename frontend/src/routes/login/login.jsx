ba8c8a3492a49a6b7c6e4ed378c75942ab8fa847
Lemon <qiwenruan11@gmail.com>
2022-10-31 00:50:16 -0400

    Created a login form

--- frontend/src/routes/login/login.jsx
+++ frontend/src/routes/login/login.jsx
@@ -1,8 +1,10 @@
+import LoginForm from "../../components/loginForm/loginForm";
 const Login = () => {
 
     return (
       <div className="login">
         <h1>this is going to be a Login form</h1>
+        <LoginForm />
       </div>
     );
   
