cd9eb3abd3ffd4df686ae1011ad4e80de47208c1
Ruby Kim <rubyruvy@gmail.com>
2022-11-06 23:17:12 -0500

    style updated

--- frontend/src/components/loginForm.jsx
+++ frontend/src/components/loginForm.jsx
@@ -2,11 +2,11 @@ import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
     return (
-        // <div className="flex h-screen m-0">
-            <div className="grid gap-8 place-content-center mx-6 h-screen">
-            <h1 className="text-2xl">Welcome back</h1>
-            {/* <div > */}
-
+        <div className="grid h-screen w-screen justify-center">
+        <div className="grid gap-y-8 place-content-center h-screen">
+            {/* <div className="place-items-center"> */}
+                <h1 className="text-2xl font-semibold">Welcome back</h1>
+            {/* <div/> */}
                 <form onSubmit={()=>{}} className="grid gap-4">
                     {/* <label> Email </label> */}
                     <div className="form-control w-full max-w-xs">
@@ -15,7 +15,7 @@ const LoginForm = () => {
                         </label>
                     <input type="text" placeholder="Email address"
                         className="input w-full max-w-xs rounded-full border-solid border-2
-                        input-sm sm:input-sm md:input-md lg:input-lg"/>
+                        input-sm sm:input-sm md:input-md lg:input-lg font-light text-xs"/>
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label">
@@ -23,7 +23,7 @@ const LoginForm = () => {
                         </label>
                     <input type="text" placeholder="Password"
                         className="input w-full max-w-xs rounded-full border-solid border-2
-                        input-sm sm:input-sm md:input-md lg:input-lg"/>
+                        input-sm sm:input-sm md:input-md lg:input-lg font-light"/>
                     </div>
                     {/* <label> Password</label>
                     <input type ="password"required />
@@ -31,14 +31,13 @@ const LoginForm = () => {
                 </form>
 
                 <Link to="/discover">
-                    <button className="text-m px-20 btn-sm sm:btn-sm md:btn-md lg:btn-lg bg-primary text-white rounded-full">
+                    <button className="text-m px-20 w-80 btn-sm sm:btn-sm md:btn-md lg:btn-lg bg-primary text-white rounded-full">
                         Sign in
                     </button>
                 </Link>
 
-            {/* </div> */}
-        </div>
-        // </div>
+            </div>
+            </div>
     )
 
 }
