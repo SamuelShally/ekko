94bdc87be4c7d2e8e82d58c5f1d80be7f86dd57f
Ruby Kim <rubyruvy@gmail.com>
2022-11-07 02:23:48 -0500

    working on layout

--- frontend/src/components/helloEkko.jsx
+++ frontend/src/components/helloEkko.jsx
@@ -20,27 +20,35 @@ const Greeting = () => {
     }, []);
  
     return (
-        <div className="max-w-screen max-h-screen">
-            <img className = "fixed mx-auto h-auto opacity-25 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" src={fish} alt="Fish" />
-            <div className = "font-readex">
-                {/* <h1 className='text-lg'>Hello {msg}</h1> */}
-                <h1 className="absolute text-4xl text-center top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                    Connections Built on<br />Exchange of Ideas.
-                </h1>
-            </div> 
-            <Navigation></Navigation>
-            <Link to="/learn">
-                <button className="btn absolute btn-xs top-1/2 right-1/3 sm:btn-sm md:btn-md lg:btn-lg bg-secondary text-neutral rounded-full">
-                    Learn
-                </button>
-            </Link>
-            <Link to="/discover">
-                <button className="btn absolute btn-xs top-1/2 right-1/3 sm:btn-sm md:btn-md lg:btn-lg bg-secondary text-neutral rounded-full">
-                    Discover
-                </button>
-            </Link>
+        <div className="relative w-screen h-screen">
+            <div className="relative">
+                <Navigation/>
+            </div>
+            {/* <img className = "fixed mx-auto h-auto opacity-25 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" src={fish} alt="Fish" /> */}
+            <div className='absolute w-screen top-1/3 md:top-2'>
+                <div className="relative">
+                <img className="absolute opacity-25 w-3/4 md:w-1/3 lg:w-2/5 left-1/2 -translate-x-1/2" src={fish} alt="Fish" />
+                <div className="relative grid gap-y-10 md:top-1/2 lg:top-1/2">
+                    <h1 className="font-readex text-4xl text-center justify-center">
+                        Connections Built on<br />Exchange of Ideas.
+                    </h1>  
+                    <div className='grid grid-cols-2 justify-items-center'>
+                        <Link to="/learn">
+                            <button className="text-xl justify-items-center btn btn-md sm:btn-sm md:btn-md lg:btn-lg bg-secondary text-neutral rounded-full">
+                                Learn
+                            </button>
+                        </Link>
+                        <Link to="/discover">
+                            <button className="text-xl justify-items-center btn btn-md sm:btn-sm md:btn-md lg:btn-lg bg-secondary text-neutral rounded-full">
+                                Discover
+                            </button>
+                        </Link>
+                    </div>
+                </div>
+                </div>
+                
+        </div>
             
-
         </div>
     )
 
