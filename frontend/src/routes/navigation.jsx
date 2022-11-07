c5044e1ccd7748ac204afeabd75856cf045d8c5d
Ruby Kim <rubyruvy@gmail.com>
2022-11-07 02:18:50 -0500

    restructured, styles added

--- frontend/src/routes/navigation.jsx
+++ frontend/src/routes/navigation.jsx
@@ -8,63 +8,36 @@ const Navigation = () => {
     // <nav className="w-full flex py-6 justify-between items-center">
     //   <img src={logo} alt="ekko" className="w-[250px] h-[124px]"/>
     // </nav>
-    <div className="navbar bg-base-100 w-full flex py-6 justify-between items-center">
+    <div className="navbar bg-base-100 w-full flex p-3 justify-between items-center"> 
       <div className="flex-1">
-      {/* <img src={logo} alt="ekko" /> */}
-        <a className="btn btn-ghost normal-case text-xl">Ekko</a>
+        {/* <img src={logo} alt="ekko" /> */}
+        <Link to="/">
+          <button className="btn btn-ghost normal-case text-2xl px-2">
+            Ekko
+          </button>
+        </Link>
       </div>
-      <div className="flex-none">
-        <ul className="menu menu-horizontal p-0">
+      <div className="flex-none p-2">
+        <ul className="menu menu-horizontal">
           <li>
-            <Link to="/login"><button className="sm:btn-sm md:btn-md lg:btn-lg btn-secondary">Sign in</button></Link>
+            <Link to="/login">
+              <button className="text-l btn btn-sm sm:btn-sm md:btn-md lg:btn-lg py-0">
+                Sign in
+              </button>
+            </Link>
           </li>
           <li>
-            <Link to="/register"><button className="sm:btn-sm md:btn-md lg:btn-lg bg-primary text-neutral rounded-full">Sign up</button></Link>
+            <Link to="/register">
+              <button className="text-m btn btn-sm sm:btn-sm md:btn-md lg:btn-lg bg-primary text-neutral rounded-full hover:text-secondary hover:bg-white">
+                Sign up
+              </button>
+            </Link>
           </li>
         </ul>
       </div>
     </div>
   );
 };
-
-// const Navigation = () =>{
-//     return (
-//       <Fragment>
-//         <div className = "navigation">
-//         <div className = "home-logo">
-//         <Link className = "home-link" to='/'>
-//             Ekko 
-//        </Link>
-//         </div>
-
-//         <div className = "register-component">
-//         <Link className = "register-link" to='/register'>
-//             Register 
-//        </Link>
-//         </div>
-
-//         <div className = "login-component">
-//         <Link className = "login-link" to='/login'>
-//             Login 
-//        </Link>
-//         </div>
-
-//         <div className = "learn-component">
-//         <Link className = "learn-link" to='/learn'>
-//             Learn 
-//        </Link>
-//         </div>
-
-//         <div className = "discover-component">
-//         <Link className = "discover-link" to='/discover'>
-//             Discover 
-//        </Link>
-//         </div>
-//         <Outlet/>
-//         </div>
-//       </Fragment>
-//     );
-//   };
 
 export default Navigation;
 
