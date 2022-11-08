1bd96341dd55d2d74638ad8b00b5cdb04a1f3a82
Ruby Kim <rubyruvy@gmail.com>
2022-11-08 10:38:05 -0500

    updated layout and styles

--- frontend/src/components/CreateProfile.jsx
+++ frontend/src/components/CreateProfile.jsx
@@ -2,37 +2,40 @@ import { Link } from 'react-router-dom';
 
 const CreateProfile = () => {
     return (
-        <div className="relative h-screen">
-            <h1 className="absolute w-screen my-20 text-4xl text-primary text-center text-bold">Create profile</h1>
-            <div className="absolute card card-bordered rounded-3xl bg-accent left-1/2 -translate-x-1/2 shadow-xl top-1/2 -translate-y-1/2
-                            w-3/4 lg:card-side">
-                <img className="rounded-3xl mt-10 mx-8" src="https://placeimg.com/400/225/arch" alt="User1"/>
-                <div className="relative card-body items-center text-center text-primary">
-                    {/* <h2 className="card-title text-3xl mt-8">User1</h2> */}
-                    <div className="w-full mt-8">
-                        <input type="text" placeholder="Enter username" className="input input-bordered rounded-full max-w-xs" />
-                    </div>
-                    <div className="form-control mt-4">
-                        {/* <label className="label">
-                            <span className="label-text text-sm text-stone-400 font-light mb-1">Pick one!</span>
-                        </label> */}
-                        <select className="select select-bordered rounded-full px-6">
-                            <option disabled selected>Choose your worldview</option>
-                            <option>worldview1</option>
-                            <option>worldview2</option>
-                            <option>worldview3</option>
-                            <option>worldview4</option>
-                            <option>other</option>
-                        </select>
+        <div className="relative h-screen bg-accent">
+            <div className="absolute grid h-3/4 inset-y-20 bg-accent place-items-center">
+                <h1 className="relative w-screen  text-4xl text-primary text-center text-bold">Create profile</h1>
+                <div className="relative card card-bordered rounded-3xl bg-neutral shadow-xl
+                                w-3/4 lg:card-side">
+                    <img className="rounded-3xl mt-10 mx-8" src="https://placeimg.com/400/225/arch" alt="User1"/>
+                    <div className="relative gap-y-2 card-body items-center text-center text-primary">
+                        {/* <h2 className="card-title text-3xl mt-8">User1</h2> */}
+                        <div className="w-full mt-8">
+                            <input type="text" placeholder="Enter username" className="input input-bordered rounded-full max-w-xs" />
                         </div>
-                    <Link to="/"> {/* remember: change it to my dashboard */}
-                        <button className="btn my-8 rounded-full 
-                                            bg-primary text-neutral text-xl">
-                            Create
-                        </button>
-                    </Link>
+                        <div className="form-control mt-4">
+                            {/* <label className="label">
+                                <span className="label-text text-sm text-stone-400 font-light mb-1">Pick one!</span>
+                            </label> */}
+                            <select className="select select-bordered rounded-full px-6">
+                                <option disabled selected>Choose your worldview</option>
+                                <option>worldview1</option>
+                                <option>worldview2</option>
+                                <option>worldview3</option>
+                                <option>worldview4</option>
+                                <option>other</option>
+                            </select>
+                            </div>
+                        <Link to="/interest"> 
+                            <button className="btn my-8 rounded-full 
+                                                bg-primary text-neutral text-xl">
+                                Next
+                            </button>
+                        </Link>
+                    </div>
                 </div>
             </div>
+            
         </div>
     )
 }
