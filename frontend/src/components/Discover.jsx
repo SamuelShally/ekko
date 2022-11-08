90192c5226bbf7f3b3c1308596e7d353ef88ea8a
Ruby Kim <rubyruvy@gmail.com>
2022-11-08 12:12:20 -0500

    updated layout

--- frontend/src/components/Discover.jsx
+++ frontend/src/components/Discover.jsx
@@ -4,21 +4,23 @@ import userImageHolder from '../img/user-img-holder.png';
 const Discover = () => {
     return (
         <div className="relative h-screen">
-            <h1 className="absolute w-screen my-24 text-4xl text-primary text-center text-bold">Discover</h1>
-            <div className="absolute card card-bordered rounded-3xl bg-accent left-1/2 -translate-x-1/2 shadow-xl top-1/2 -translate-y-1/2
-                            w-3/4 lg:card-side">
-                {/* <figure className="px-10 pt-10"> */}
-                    <img className="rounded-3xl mt-10 mx-8" src={userImageHolder} alt="User2"/>
-                {/* </figure> */}
-                <div className="card-body items-center text-center text-primary">
-                    <h2 className="card-title text-2xl mt-8">User 2</h2>
-                    <h2 className="card-title text-2xl mt-6">User 2's worldview</h2>
-                    <Link to="/"> {/* change it to chatroom */}
-                        <button className="btn align-middle my-8 rounded-full 
-                                            bg-primary text-neutral text-xl">
-                            Connect
-                        </button>
-                    </Link>
+            <div className="absolute grid h-3/4 inset-y-20 place-items-center">
+                <h1 className="relative w-screen text-4xl text-primary text-center text-bold">Discover</h1>
+                <div className="relative card card-bordered rounded-3xl bg-accent
+                                w-3/4 lg:card-side">
+                    {/* <figure className="px-10 pt-10"> */}
+                        <img className="rounded-3xl mt-10 mx-8" src={userImageHolder} alt="User2"/>
+                    {/* </figure> */}
+                    <div className="relative gap-y-2 card-body items-center text-center text-primary">
+                        <h2 className="card-title text-2xl mt-8">User 2</h2>
+                        <h2 className="card-title text-2xl mt-6">User 2's worldview</h2>
+                        <Link to="/"> {/* change it to chatroom */}
+                            <button className="btn align-middle my-8 rounded-full 
+                                                bg-primary text-neutral text-xl">
+                                Connect
+                            </button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
