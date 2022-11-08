a8d12810cfbc7f558b5df1c5f2518273e8efa032
Lemon <qiwenruan11@gmail.com>
2022-11-07 20:13:06 -0500

    created CRUD

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -9,27 +9,93 @@ router.get('/getUsers',(req,res)=>{
     // console.log(User);
     User.find({}, (err, users) => {
         if (err) {
-            res.status(400).send();
+            res.status(400).json({error: err.message});
         }
-        
         res.status(200).json(users);
     });
 });
 
-router.get('/getUser',(req,res)=>{
-    res.send("<h1>get one user</h1>");
-});
-
-router.post('/createUser',(req,res)=>{
-    const {username,email,password} = req.body;
-    try{
-        const newUser = User.create({username,email,password});
-        //conaole.log(newUser);
-        res.status(200).json(newUser);
-    }catch(err){
-        res.status(400).json({error:error.message});
+router.get('/getUser/:id',(req,res)=>{
+    
+    const {id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"User not found"})
+    }
+    const user = User.findById(id,(err,oneUser)=>{
+        if (err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        
+        res.status(200).json(oneUser)
+    })
+    if(!user){
+        return res.status(404).json({error:"No such user"});
     }
     
 });
+
+router.post('/', (req,res)=>{
+    const {username,email,password} = req.body;
+
+    User.create({username,email,password}, (err, user) => {
+        if (err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        console.log(user);
+        res.status(200).json(user);
+
+    });    
+});
+
+router.delete('/getUser/:id',(req,res)=>{
+    const {id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"User not found"})
+    }
+    const user = User.findOneAndDelete({_id:id},(err,oneUser)=>{
+        if (err) {
+            res.status(400).json({error: err.message});
+            return;
+        }
+        
+        res.status(200).json(oneUser);
+
+    });
+
+    if(!user){
+        return res.status(404).json({error:"No such user"});
+    }
+
+
+});
+
+router.patch('/getUser/:id',(req,res)=>{
+    const {id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"User not found"})
+    }
+
+    const user = User.findOneAndUpdate(
+        {_id:id},
+        {...req.body},(err,data)=>{
+            if(err){
+                res.status(400).json({error: err.message});
+                return;
+            }
+            res.status(200).json(data);
+
+        });
+    
+    if(!user){
+            return res.status(404).json({error:"No such user"});
+    }
+
+
+
+})
+
+
 
 module.exports = router;
