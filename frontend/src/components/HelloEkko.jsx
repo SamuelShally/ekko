6e49186af919aed0bf3d6a9996ccef5981d3fe40
Ruby Kim <rubyruvy@gmail.com>
2022-11-09 17:23:11 -0500

    footer overlapping issue resolved

--- frontend/src/components/HelloEkko.jsx
+++ frontend/src/components/HelloEkko.jsx
@@ -7,42 +7,44 @@ import ekkoO from '../img/ekko-o.svg';
 const Greeting = () => {
  
     return (
-        <div className="relative w-screen z-0 scroll-smooth">
+        <div className="relative w-screen h-screen z-0 mb-36">
             {/* navigation bar */}
             <div className="relative z-30">
                 <Navigation/>
             </div>
 
             {/* main wrapper */}
-            <div className='absolute w-screen z-20'>
-                <div className="relative inset-y-36">
+            <div className='relative'>
+                <div className='absolute w-screen z-20'>
+                    <div className="relative inset-y-36">
 
-                    {/* fish img + msg */}
-                    <div className="absolute">
-                        <img className="relative opacity-25 left-1/2 -translate-x-1/2
-                                        w-2/3 sm:w-2/3" src={fish} alt="Fish"/>
-                        <div className="absolute top-1/2 -translate-y-1/2">
-                            <h1 className="font-readex text-4xl text-left leading-normal px-6 w-full">
-                                Connections<br /><span className="font-light">built on</span><br />Exchange of Ideas
-                            </h1>  
+                        {/* fish img + msg */}
+                        <div className="absolute">
+                            <img className="relative opacity-25 left-1/2 -translate-x-1/2
+                                            w-2/3 sm:w-2/3" src={fish} alt="Fish"/>
+                            <div className="absolute top-1/2 -translate-y-1/2">
+                                <h1 className="font-readex text-4xl text-left leading-normal px-6 w-full">
+                                    Connections<br /><span className="font-light">built on</span><br />Exchange of Ideas
+                                </h1>  
+                            </div>
                         </div>
-                    </div>
 
-                    {/* Learn & Discover buttons */}
-                    <div className='absolute grid grid-rows-2 gap-y-8 justify-items-start 
-                                    mt-80 pb-20 pt-10'>
-                        <Link to="/learn">
-                            <button className="text-2xl px-8 ml-6 btn btn-lg md:btn-md lg:btn-lg bg-secondary text-neutral rounded-full">
-                                Learn
-                            </button>
-                        </Link>
-                        <Link to="/discover">
-                            <button className="text-2xl ml-6 px-10 btn btn-lg md:btn-md lg:btn-lg bg-secondary text-neutral rounded-full">
-                                Discover
-                            </button>
-                        </Link>
-                    </div>
-                </div>  
+                        {/* Learn & Discover buttons */}
+                        <div className='absolute grid grid-rows-2 gap-y-8 justify-items-start 
+                                        mt-80 mb-20 pt-10'>
+                            <Link to="/learn">
+                                <button className="text-2xl px-8 ml-6 btn btn-lg md:btn-md lg:btn-lg bg-secondary text-neutral rounded-full">
+                                    Learn
+                                </button>
+                            </Link>
+                            <Link to="/discover">
+                                <button className="text-2xl ml-6 px-10 btn btn-lg md:btn-md lg:btn-lg bg-secondary text-neutral rounded-full">
+                                    Discover
+                                </button>
+                            </Link>
+                        </div>
+                    </div> 
+                </div>
             </div>
         </div>
     )
