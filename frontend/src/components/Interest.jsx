73b0a39142a87587b243074bc1eb05e098e7f8d2
Ruby Kim <rubyruvy@gmail.com>
2022-11-09 15:57:57 -0500

    side margins synced

--- frontend/src/components/Interest.jsx
+++ frontend/src/components/Interest.jsx
@@ -1,17 +1,18 @@
 import { Link } from 'react-router-dom';
+// left & right margins: 6
 
 const Interest = () => {
     return (
         <div className="bg-white">
             <div className="relative h-screen top-20">
                 {/* question */}
-                <h1 className="relative font-bold text-3xl text-primary mb-10 ml-4">
+                <h1 className="relative font-bold text-3xl text-primary mb-10 ml-6">
                     Which social issues<br />spark your interest?</h1>
-                {/* option wrapper div */}
+                {/* option-wrapper div */}
                 <div className="relative bg-neutral h-full rounded-tl-3xl rounded-tr-3xl">
                     <h2 className="text-secondary text-xl font-light pt-10 mb-6 ml-4">Choose up to 3</h2>
                     {/* options */}
-                    <div className="grid gap-6 inset-x-7 ml-4">
+                    <div className="grid gap-6 inset-x-7 ml-6">
                         <button className="btn bg-secondary btn-md rounded-full w-5/12 
                                             text-xl text-left text-neutral justify-start
                                             sm:btn-md md:btn-md lg:btn-lg">+ election
@@ -39,7 +40,7 @@ const Interest = () => {
                     </div>
                     <Link to="/quiz"> 
                         <div className="w-full flex flex-row-reverse mt-10">
-                            <button className="btn rounded-full flex-none mr-4 
+                            <button className="btn rounded-full flex-none mr-6 
                                                 bg-primary text-neutral text-xl">
                                 Next
                             </button>
