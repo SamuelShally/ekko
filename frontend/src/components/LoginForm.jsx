a7c2d3d31ff9218b1fb2787d9cd49942d3ba77ae
Ruby Kim <rubyruvy@gmail.com>
2022-11-09 14:59:16 -0500

    layout testing

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -5,8 +5,13 @@ import PreviousBtn from '../components/Previous';
 const LoginForm = () => {
     return (
         <div className="relative">
-            {/* <div className="relative z-20"><img src={ekkoO} className="relative z-10 w-1/4 place-content-center ml-4 mt-4"/></div> */}
-            <div className="relative z-0 grid gap-y-16 place-content-center h-screen">
+            <div className="absolute flex justify-between place-items-center w-screen z-30 py-5">
+                <h1 className="ml-4">Prev</h1>
+                <svg xmlns="http://www.w3.org/2000/svg" className="w-20 h-16 scale-100" fill="none"><path fill="#DEDBC0" d="M35.786 35.009c14.869-7.167-20.51 1.162 3.099-32.858C56.669.955 66.606 13.566 67.84 30.798c1.234 17.23-9.178 32.354-24.112 32.354-6.86-11.568-17.532-21.331-7.942-28.143Z"/><path fill="#29180F" d="M49.715 29.833c-.426 2.318-2.25 4.048-3.969 5.657-6.642 6.218-11.253 13.861-1.413 27.662-21.517 1.447-40.156-10.76-41.39-27.99C1.707 17.93 18.577 3.597 40.094 2.15c-14.341 10.19 11.774 15.96 9.62 27.682Z"/></svg>
+            </div>
+            <div className="relative z-0 grid gap-y-14 place-content-center h-screen ">
+                {/* <svg xmlns="http://www.w3.org/2000/svg" className="w-20 h-16 scale-125 justify-self-end" fill="none"><path fill="#DEDBC0" d="M35.786 35.009c14.869-7.167-20.51 1.162 3.099-32.858C56.669.955 66.606 13.566 67.84 30.798c1.234 17.23-9.178 32.354-24.112 32.354-6.86-11.568-17.532-21.331-7.942-28.143Z"/><path fill="#29180F" d="M49.715 29.833c-.426 2.318-2.25 4.048-3.969 5.657-6.642 6.218-11.253 13.861-1.413 27.662-21.517 1.447-40.156-10.76-41.39-27.99C1.707 17.93 18.577 3.597 40.094 2.15c-14.341 10.19 11.774 15.96 9.62 27.682Z"/></svg> */}
+                {/* <div className="h-0"></div> */}
                 <h1 className="text-3xl font-semibold">Welcome back</h1>
                 <form onSubmit={()=>{}} className="grid gap-6">
                     <div className="form-control w-full max-w-xs">
