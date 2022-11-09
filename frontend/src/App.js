349ccb3f77bcc73a10cc2a8d6c6ed0012e3aff89
Ruby Kim <rubyruvy@gmail.com>
2022-11-09 11:29:01 -0500

    QuizOne component created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import Home from './routes/Home';
 import Register from './routes/Register';
 import CreateProfile from './components/CreateProfile';
 import Interest from './components/Interest';
+import QuizOne from './components/QuizOne';
 import Navigation from './routes/Navigation';
 import Login from './routes/Login';
 import Discover from './components/Discover';
@@ -23,7 +24,8 @@ const App = () => {
         <Route path='/nav' element = {<Navigation />} />
         <Route path='/register' element={<Register />} />
         <Route path='/profile' element={<CreateProfile />} />
-        <Route path='/interest' element = {<Interest />} />
+        <Route path='/interest' element={<Interest />} />
+        <Route path='/quiz-1' element = {<QuizOne />} />
         <Route path='/login' element = {<Login />} />
         <Route path='/learn' element = {<Learn />} />
         <Route path='/discover' element = {<Discover />} />
