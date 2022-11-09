6a6a62c126b876263bc07ee90e730bfcea05c751
Ruby Kim <rubyruvy@gmail.com>
2022-11-09 17:22:58 -0500

    arrow svg

--- frontend/src/components/Previous.jsx
+++ frontend/src/components/Previous.jsx
@@ -1,7 +1,9 @@
 const Previous = () => {
     return (
         <div>
-            <h1 className="ml-4">Prev</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 m-4">
+                <path fillRule="evenodd" d="M7.72 12.53a.75.75 0 010-1.06l7.5-7.5a.75.75 0 111.06 1.06L9.31 12l6.97 6.97a.75.75 0 11-1.06 1.06l-7.5-7.5z" clipRule="evenodd" />
+            </svg>
         </div>
     )
 }
