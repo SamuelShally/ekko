2aa50dfb3690c9d8765147110d235084d0e68bbe
Lemon <qiwenruan11@gmail.com>
2022-11-12 18:14:38 -0500

    added md5 to patching

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,11 +13,14 @@ router.get('/getUsers',(req,res)=>{
         }
         res.status(200).json(users);
     });
+  
+    
 });
 
 router.get('/getUser/:id',(req,res)=>{
-    
+   
     const {id} = req.params;
+    
     if(!mongoose.Types.ObjectId.isValid(id)){
         return res.status(404).json({error:"User not found"})
     }
@@ -37,15 +40,14 @@ router.get('/getUser/:id',(req,res)=>{
 
 router.post('/', (req,res)=>{
     const {username,email,password} = req.body;
-    console.log(req.body);
 
     //encrypt the password before saving it
     let encrypedPW = md5(password);
-    console.log(encrypedPW)
-    req.body.password = encrypedPW;
+  
+  
     
     //save a new user with encrypted password
-    User.create({username,email, password}, (err, user) => {
+    User.create({username,email, password: encrypedPW}, (err, user) => {
         if (err) {
             res.status(400).json({error: err.message});
             return;
@@ -86,9 +88,22 @@ router.patch('/getUser/:id',(req,res)=>{
         return res.status(404).json({error:"User not found"})
     }
 
+    const data = {
+        _id: id,
+    }
+    if(req.body.username) {
+        data.username = req.body.username;
+    }
+    if(req.body.email) {
+        data.email = req.body.email;
+    }
+    if(req.body.password) {
+        data.password = md5(req.body.password);
+    }
+
     const user = User.findOneAndUpdate(
         {_id:id},
-        {...req.body},(err,data)=>{
+        data,(err,data)=>{
             if(err){
                 res.status(400).json({error: err.message});
                 return;
