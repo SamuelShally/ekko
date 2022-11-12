530684e67e60857fcb426b9e04c12c40f4f0ae76
Lemon <qiwenruan11@gmail.com>
2022-11-12 17:42:22 -0500

    merged Testing branch's code. currently fixing some bugs

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema(
             unique:true,
             required: true
         },
-        encrypedPW: {
+        password: {
             type:String,
             required: true
             
