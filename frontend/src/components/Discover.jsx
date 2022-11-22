7ab8d50b9b5a1c5d7bd3f3bf15f66b7c8c8e79bd
CharlotteHuliyi <CharlotteHuliyi@gmail.com>
2022-11-22 22:29:33 +0800

    User's world view and chat room

--- frontend/src/components/Discover.jsx
+++ frontend/src/components/Discover.jsx
@@ -1,23 +1,52 @@
 import { Link } from 'react-router-dom';
+import userImageHolder from '../img/user-img-holder.png';
+import React, { useState, useEffect } from 'react';
+
 const Discover = () => {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:4000/api/users/getUsers')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setUsers(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
     return (
         <div className="relative h-screen">
-            <h1 className="absolute w-screen my-24 text-4xl text-primary text-center text-bold">Discover</h1>
-            <div className="absolute card card-bordered rounded-3xl bg-accent left-1/2 -translate-x-1/2 shadow-xl top-1/2 -translate-y-1/2
-                            w-3/4 lg:card-side">
-                {/* <figure className="px-10 pt-10"> */}
-                    <img className="rounded-3xl mt-10 mx-8" src="https://placeimg.com/400/225/arch" alt="User1"/>
-                {/* </figure> */}
-                <div className="card-body items-center text-center text-primary">
-                    <h2 className="card-title text-2xl mt-8">User 2</h2>
-                    <h2 className="card-title text-2xl mt-6">User 2's worldview</h2>
-                    <Link to="/"> {/* change it to chatroom */}
-                        <button className="btn align-middle my-8 rounded-full 
-                                            bg-primary text-neutral text-xl">
-                            Connect
-                        </button>
-                    </Link>
-                </div>
+            <div className="absolute grid h-3/4 inset-y-20 place-items-center">
+                <h1 className="relative w-screen text-4xl text-primary text-center text-bold">Discover</h1>
+                {
+                    users.map((user, index) => {
+                        return (
+                            <div key={index} className="relative card card-bordered rounded-3xl bg-accent
+                                w-3/4 lg:card-side">
+                                {/* <figure className="px-10 pt-10"> */}
+                                <img className="rounded-3xl mt-10 mx-8" src={userImageHolder} alt="User2" />
+                                {/* </figure> */}
+                                <div className="relative gap-y-2 card-body items-center text-center text-primary">
+                                    <h2 className="card-title text-2xl mt-8">{user.username}</h2>
+                                    <h2 className="card-title text-2xl mt-6">{user.email}</h2>
+                                    <h3 className='card-title text-1xl mt-6'>{user.worldview}</h3>
+                                    <Link to={"/chat/"+user.username}> {/* change it to chatroom */}
+                                        <button className="btn align-middle my-8 rounded-full 
+                                                bg-primary text-neutral text-xl">
+                                            Connect
+                                        </button>
+                                    </Link>
+                                </div>
+                            </div>
+                        )
+
+                    })
+                }
+
             </div>
         </div>
 
--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -1,33 +1,97 @@
 import { Link } from 'react-router-dom';
-import ekkoO from '../img/ekko-o-512.png';
+import Previous from '../components/Previous';
+import { useState } from 'react';
+import React from 'react';
+import { useNavigate  } from 'react-router-dom';
 
 const RegisterForm = () => {
+    const [username,setUsername] = useState('');
+    const [email,setEmail] = useState('');
+    const [password,setPassword]=useState('');
+    const [error,setError] = useState(null);
+    const navigate = useNavigate();
+
+    const submitForm= async(e) =>{
+        e.preventDefault();
+        const user = {username,email,password};
+        const response = await fetch("http://localhost:4000/api/users/signup",{
+            method:'POST',
+            body: JSON.stringify(user),
+            headers:{
+                'Content-Type' : "application/json"
+            }
+        });
+        console.log(response);
+        const json = await response.json();
+        if(!response.ok){
+            console.log(json.error);
+            setError(json.error);
+        }
+        if(response.ok){
+            setUsername('');
+            setEmail('');
+            setPassword('');
+            setError(null);
+            console.log("New User Added");
+            navigate('/profile'); //navigate to creating user profile
+
+        }
+
+    }
+
+
+
     return (
-        <div>
+        <div className="relative">
             {/* <div className="relative z-20"><img src={ekkoO} className="relative z-10 w-1/4 place-content-center ml-4 mt-4"/></div> */}
-            <div className="relative z-0 grid gap-y-16 place-content-center h-screen">
+            <div className="absolute flex justify-between place-items-center w-screen z-30 py-5">
+                <Link to="/">
+                    <Previous/>
+                </Link>
+            </div>
+            <div className="relative z-0 grid w-screen h-screen gap-y-14 place-content-center">
+                <div className="h-0 place-self-end">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-20 h-16 scale-75 justify-self-end -mr-4" fill="none"><path fill="#DEDBC0" d="M35.786 35.009c14.869-7.167-20.51 1.162 3.099-32.858C56.669.955 66.606 13.566 67.84 30.798c1.234 17.23-9.178 32.354-24.112 32.354-6.86-11.568-17.532-21.331-7.942-28.143Z"/><path fill="#29180F" d="M49.715 29.833c-.426 2.318-2.25 4.048-3.969 5.657-6.642 6.218-11.253 13.861-1.413 27.662-21.517 1.447-40.156-10.76-41.39-27.99C1.707 17.93 18.577 3.597 40.094 2.15c-14.341 10.19 11.774 15.96 9.62 27.682Z"/></svg>
+                </div>
                 <h1 className="text-3xl font-semibold">Get started</h1>
-                <form onSubmit={()=>{}} className="grid gap-6">
-                    <div className="form-control w-full max-w-xs">
+                <form  className="grid gap-6">
+                    <div className="form-control w-3/4 max-w-xs">
+                        <label className="label"/>
+                    <input type="text" placeholder="Username"
+                            className="input w-full max-w-xs rounded-full border-solid border-2
+                            input-md md:input-lg lg:input-lg font-light text-xs"
+                            value={username}
+                            onChange={(e)=>setUsername(e.target.value)}
+                            />
+                    </div>
+                    <div className="form-control w-80 max-w-xs">
                         <label className="label"/>
                     <input type="text" placeholder="Email address"
                             className="input w-full max-w-xs rounded-full border-solid border-2
-                            input-md md:input-lg lg:input-lg font-light text-xs"/>
+                            input-md md:input-lg lg:input-lg font-light text-xs"
+                            value={email}
+                            onChange={(e)=>setEmail(e.target.value)}/>
                     </div>
                     <div className="form-control w-full max-w-xs">
                         <label className="label"/>
-                        <input type="text" placeholder="Set password"
+                        <input type="password" placeholder="Set password"
                             className="input w-full max-w-xs rounded-full border-solid border-2
-                            input-md md:input-lg lg:input-lg font-light text-xs"/>
+                            input-md md:input-lg lg:input-lg font-light text-xs"
+                            value={password}
+                            onChange={(e)=>setPassword(e.target.value)}/>
                     </div>
-                </form>
-                <Link to="/profile">
+                    
                     <button
-                        className="text-l text-white rounded-full bg-primary
-                        px-20 w-80 btn-md md:btn-lg lg:btn-lg">
-                            Create an account
+                        className="text-lg text-white rounded-full bg-primary
+                        px-20 w-80 btn-md md:btn-lg lg:btn-lg"
+                        onClick={submitForm}
+                    >
+                        Create an account
                     </button>
-                </Link>
+                    {error && <div className="error">{error}</div>}
+                
+                </form>
+               
                 
             </div>
         </div>
