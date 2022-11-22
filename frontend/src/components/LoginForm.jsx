0261b7a94b641973e24c1f33b58e7c62d0657b26
Ruby Kim <rubyruvy@gmail.com>
2022-11-22 13:05:53 -0500

    removed blocker

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -2,7 +2,7 @@ import { Link, useSearchParams } from 'react-router-dom';
 import PreviousBtn from '../components/Previous';
 import { useState } from 'react';
 import { useNavigate  } from 'react-router-dom';
-import { Error } from 'mongoose';
+// import { Error } from 'mongoose';
 import Previous from '../components/Previous';
 
 
