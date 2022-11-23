dff1d52ea90b1dafaa622540492a79bcb44403d8
Ruby Kim <rubyruvy@gmail.com>
2022-11-22 23:06:03 -0500

    BlogPost component created

---
+++ frontend/src/components/BlogPost.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+const Post = () => {
+    return ( 
+        <div>
+            {/* <div className='text-3xl'>Title</div> */}
+            <input type="text" placeholder="Title" className="input w-full mt-20 text-xl border-primary border-y-2 border-opacity-10" />
+            <textarea className="textarea textarea-bordered border-primary border-y-2 border-opacity-10
+                                mt-4 w-full h-96 text-lg"
+                placeholder="Type here" />
+            <button className="flex btn bg-accent rounded-full text-primary ml-2">Post</button>
+        </div>
+    )
+}
+
+export default Post;
