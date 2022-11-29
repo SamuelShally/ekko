050e469051fa9ea42eafb784edcf5fd4c8f5e581
Ruby Kim <rubyruvy@gmail.com>
2022-11-29 14:32:34 -0500

    added md breakpoint, split view

--- frontend/src/components/Discover.jsx
+++ frontend/src/components/Discover.jsx
@@ -4,24 +4,33 @@ import userImageHolder from '../img/user-img-holder.png';
 const Discover = () => {
     return (
         <div className="relative h-screen">
-            <div className="absolute grid h-3/4 inset-y-20 place-items-center">
-                <h1 className="relative mb-6 w-screen text-4xl text-primary text-center text-bold">Discover</h1>
-                <div className="relative card card-bordered rounded-3xl bg-accent
-                                max-w-sm w-3/4 lg:card-side">
-                    {/* <figure className="px-10 pt-10"> */}
-                        <img className="rounded-3xl mt-10 mx-8" src={userImageHolder} alt="User2"/>
-                    {/* </figure> */}
-                    <div className="relative gap-y-2 card-body items-center text-center text-primary">
-                        <h2 className="card-title text-2xl mt-8">User 2</h2>
-                        <h2 className="card-title text-2xl mt-6">User 2's worldview</h2>
-                        <Link to="/"> {/* change it to chatroom */}
-                            <button className="btn align-middle my-8 rounded-full 
-                                                bg-primary text-neutral text-xl">
-                                Connect
-                            </button>
-                        </Link>
+            <div className='grid grid-cols-3'>
+                {/* block on breakpoint md*/}
+                <div className='relative hidden bg-neutral text-primary h-screen
+                                md:block md:col-span-1'>
+                    <h1 className="absolute left-0 ml-10 mt-40 text-4xl text-primary text-center text-bold">Discover</h1>
+                </div>
+                <div className="h-screen w-screen inset-y-20 mt-40 itmes-center 
+                                md:w-full md:col-span-2">
+                    <h1 className="relative mb-6 w-screen text-4xl text-primary text-center text-bold md:hidden">Discover</h1>
+                    <div className="relative card card-bordered rounded-3xl bg-accent
+                                    max-w-sm mx-auto w-3/4 ">
+                        {/* <figure className="px-10 pt-10"> */}
+                            <img className="rounded-3xl mt-10 mx-8" src={userImageHolder} alt="User2"/>
+                        {/* </figure> */}
+                        <div className="relative gap-y-2 card-body items-center text-center text-primary">
+                            <h2 className="card-title text-2xl mt-8 mx-4">User 2</h2>
+                            <h2 className="card-title text-2xl mt-6 mx-4">User 2's worldview</h2>
+                            <Link to="/"> {/* change it to chatroom */}
+                                <button className="btn align-middle my-8 mx-4 rounded-full 
+                                                    bg-primary text-neutral text-xl">
+                                    Connect
+                                </button>
+                            </Link>
+                        </div>
                     </div>
                 </div>
+
             </div>
         </div>
 
