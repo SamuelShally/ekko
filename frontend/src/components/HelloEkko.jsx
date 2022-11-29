b4d65b2e91d660b8d31057098bb308b434f0be69
Ruby Kim <rubyruvy@gmail.com>
2022-11-29 13:20:15 -0500

    make it responsive

--- frontend/src/components/HelloEkko.jsx
+++ frontend/src/components/HelloEkko.jsx
@@ -15,14 +15,17 @@ const Greeting = () => {
 
             {/* big wrapper */}
             <div className='relative'>
-                <div className='absolute w-screen h-screen z-20 mb-20'>
-                    <div className="relative inset-y-36">
+                <div className='absolute w-screen h-screen z-20'>
+                    <div className="relative inset-y-24">
                         {/* fish img + msg */}
-                        <div className="absolute">
-                            <img className="relative opacity-25 left-1/2 -translate-x-1/2
-                                            w-2/3 sm:w-2/3" src={fish} alt="Fish"/>
+                        <div className="absolute w-screen mb-10">
+                            <img className="relative opacity-25 
+                                            left-10 w-72
+                                            sm:w-80 sm:left-0 sm:mx-auto"
+                                 src={fish} alt="Fish" />
                             <div className="absolute top-1/2 -translate-y-1/2">
-                                <h1 className="font-readex text-4xl text-left leading-normal px-6 w-full">
+                                <h1 className="w-screen font-readex text-4xl text-left leading-normal px-6 
+                                               sm:text-5xl sm:leading-relaxed sm:text-center">
                                     Connections<br /><span className="font-light">built on</span><br />Exchange of Ideas
                                 </h1>  
                             </div>
@@ -30,17 +33,26 @@ const Greeting = () => {
 
                         {/* Learn, Discover  */}
                         <div className='absolute grid grid-rows-2 gap-y-8 justify-items-start 
-                                                mt-80 pb-32 pt-10'>
-                            <Link to="/learn">
-                                <button className="text-2xl px-8 ml-6 btn btn-lg md:btn-md lg:btn-lg bg-secondary text-neutral rounded-full"
-                                        onClick={() => { window.scrollTo({top: 0, left: 0, behavior: 'smooth'}); }}>
-                                        Learn
-                                </button>
-                            </Link>
+                                        mt-80 pb-32 pt-16
+                                        sm:pt-24 sm:w-full sm:justify-items-center'>
                             <Link to="/discover">
-                                <button className="text-2xl ml-6 px-10 btn btn-lg md:btn-md lg:btn-lg bg-secondary text-neutral rounded-full"
+                                <button className="px-8 text-2xl ml-6 btn btn-lg
+                                                   sm:px-8 
+                                                   md:px-8 
+                                                   lg:px-10 lg:h-14 
+                                                   bg-secondary text-neutral rounded-full"
                                         onClick={() => { window.scrollTo({top: 0, left: 0, behavior: 'smooth'}); }}>
                                         Discover
+                                </button>
+                            </Link>
+                            <Link to="/learn">
+                                <button className="px-8 text-2xl ml-6 btn btn-lg 
+                                                   sm:px-8 
+                                                   md:px-8 
+                                                   lg:px-10 
+                                                   bg-secondary text-neutral rounded-full"
+                                        onClick={() => { window.scrollTo({top: 0, left: 0, behavior: 'smooth'}); }}>
+                                        Learn
                                 </button>
                             </Link>
                             {/* <div className='w-full h-96'>
