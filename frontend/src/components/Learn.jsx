0eeb66bbdd2911b5bdbd2300571045544cb43a23
Ruby Kim <rubyruvy@gmail.com>
2022-11-29 16:29:06 -0500

    link buttons and md breakpoint added

--- frontend/src/components/Learn.jsx
+++ frontend/src/components/Learn.jsx
@@ -1,13 +1,29 @@
 const Learn = () =>{
     return (
-        <div className="relative h-screen bg-neutral w-screen">
-            <div className="grid absolute gap-y-4 bg-neutral px-8 text-center mt-20 w-screen">
-                <h1 className="text-4xl font-bold leading-relaxed">Make<br />
-                    <span className="text-secondary text-5xl"> Free Speech </span><br/>Easy.
+        <div className="relative h-screen w-screen bg-neutral place-content-center">
+            <div className="grid absolute gap-y-4 px-8 text-center mt-20 w-full">
+                <h1 className="leading-relaxed font-bold text-4xl md:text-5xl md:leading-relaxed">Make<br />
+                    <span className="text-secondary text-5xl md:text-6xl"> Free Speech </span><br/>Easy.
                 </h1>
-                <p className="text-md font-light opacity-30 text-center w-full align-middle justify-self-center">Check out our proudly curated resources to better engage in
+                <p className="w-96 text-md font-light opacity-30 text-center justify-self-center
+                              sm:w-96 sm:text-lg
+                              md:w-96 md:text-xl">Check out our proudly curated resources to better engage in
                     civil discourse and exercise your right to freedom of speech.
                 </p>
+                <div className="mt-10 grid grid-rows-3 gap-y-8 content-center w-full justify-items-center">
+                    <button className=" 
+                    btn bg-white rounded-full w-96 sm:btn-sm md:btn-md lg:btn-lg">
+                        Link 1
+                    </button>
+                    <button className=" 
+                    btn bg-white rounded-full w-96 sm:btn-sm md:btn-md lg:btn-lg">
+                        Link 2
+                    </button>
+                    <button className=" 
+                    btn bg-white rounded-full w-96 sm:btn-sm md:btn-md lg:btn-lg">
+                        Link 3
+                    </button>
+                </div>
             </div>
             {/* <div className="bg-white">
                 <div>
@@ -16,17 +32,17 @@ const Learn = () =>{
                     <div className="grid w-32 h-20 rounded bg-secondary text-secondary-content place-content-center">3</div>
                 </div>
             </div> */}
-            {/* <div className="relative grid grid-rows-3 content-center w-screen">
+            {/* <div className="absolute grid grid-rows-3 gap-y-6 content-center w-screen justify-items-center">
                 <button className=" 
-                btn bg-accent rounded-full w-80 sm:btn-sm md:btn-md lg:btn-lg">
+                btn bg-white rounded-full w-80 sm:btn-sm md:btn-md lg:btn-lg">
                     Link 1
                 </button>
                 <button className=" 
-                btn bg-accent rounded-full w-80 sm:btn-sm md:btn-md lg:btn-lg">
+                btn bg-white rounded-full w-80 sm:btn-sm md:btn-md lg:btn-lg">
                     Link 2
                 </button>
                 <button className=" 
-                btn bg-accent rounded-full w-80 sm:btn-sm md:btn-md lg:btn-lg">
+                btn bg-white rounded-full w-80 sm:btn-sm md:btn-md lg:btn-lg">
                     Link 3
                 </button>
             </div> */}
