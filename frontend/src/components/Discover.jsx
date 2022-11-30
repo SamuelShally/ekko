ae690b10406325dd40a87806176bf3e1acf8358a
Ruby Kim <rubyruvy@gmail.com>
2022-11-29 22:38:55 -0500

    layout adjustment

--- frontend/src/components/Discover.jsx
+++ frontend/src/components/Discover.jsx
@@ -17,15 +17,16 @@ const Discover = () => {
                 <div className='relative hidden bg-neutral text-primary h-full 
                                     sm:block sm:col-span-1'>
                     <h1 className="absolute right-0 mr-8 mt-8 text-primary text-center text-bold
-                                text-3xl sm:text-3xl lg:mt-9">Discover</h1>
+                                text-3xl sm:text-3xl lg:mt-9 xl:text-4xl">Discover</h1>
                 </div>
                 {/* default */}
                 <div className="h-screen w-screen mx-auto pt-20 
-                                sm:max-w-max sm:col-span-2
+                                sm:max-w-max sm:col-span-2 
                                 ">
                     <h1 className="relative mb-6 w-screen text-4xl text-primary text-center text-bold sm:hidden">Discover</h1>
                     <div className="relative card card-bordered rounded-3xl bg-accent mt-20 
-                                    max-w-sm mx-auto w-3/4 ">
+                                    sm:max-w-sm mx-auto w-3/4
+                                    md:max-w-lg">
                         {/* <figure className="px-10 pt-10"> */}
                             <img className="rounded-3xl mt-10 mx-8" src={userImageHolder} alt="User2"/>
                         {/* </figure> */}
