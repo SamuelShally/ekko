d849dc8553cabede39faaae21aea938ac4b22ccd
Ruby Kim <rubyruvy@gmail.com>
2022-11-29 21:33:25 -0500

    prev button + responsive

--- frontend/src/components/Discover.jsx
+++ frontend/src/components/Discover.jsx
@@ -1,21 +1,30 @@
 import { Link } from 'react-router-dom';
 import userImageHolder from '../img/user-img-holder.png';
+import Previous from '../components/Previous';
 
 const Discover = () => {
     return (
-        <div className="relative h-screen bg-neutral md:bg-transparent">
-            <div className='grid grid-cols-3'>
-                {/* block on breakpoint md*/}
-                <div className='relative hidden bg-neutral text-primary h-full
-                                md:block md:col-span-1'>
-                    <h1 className="absolute left-0 ml-10 mt-40 text-4xl text-primary text-center text-bold
-                                    lg:mt-56">Discover</h1>
+        <div className="relative h-screen bg-neutral sm:bg-transparent">
+            {/* Previous button */}
+            <div className="absolute flex justify-between place-items-center w-screen z-30 py-5">
+                    <Link to="/">
+                        <Previous/>
+                    </Link>
+            </div>
+
+            <div className='grid sm:grid-cols-3'>
+                {/* block on breakpoint sm */}
+                <div className='relative hidden bg-neutral text-primary h-full 
+                                    sm:block sm:col-span-1'>
+                    <h1 className="absolute right-0 mr-8 mt-8 text-primary text-center text-bold
+                                text-3xl sm:text-3xl lg:mt-9">Discover</h1>
                 </div>
-                <div className="h-screen w-screen inset-y-20 mt-40 itmes-center 
-                                md:w-full md:col-span-2
-                                lg:mt-56">
-                    <h1 className="relative mb-6 w-screen text-4xl text-primary text-center text-bold md:hidden">Discover</h1>
-                    <div className="relative card card-bordered rounded-3xl bg-accent
+                {/* default */}
+                <div className="h-screen w-screen mx-auto pt-20 
+                                sm:max-w-max sm:col-span-2
+                                ">
+                    <h1 className="relative mb-6 w-screen text-4xl text-primary text-center text-bold sm:hidden">Discover</h1>
+                    <div className="relative card card-bordered rounded-3xl bg-accent mt-20 
                                     max-w-sm mx-auto w-3/4 ">
                         {/* <figure className="px-10 pt-10"> */}
                             <img className="rounded-3xl mt-10 mx-8" src={userImageHolder} alt="User2"/>
@@ -32,7 +41,6 @@ const Discover = () => {
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
 
