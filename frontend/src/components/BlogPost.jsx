c67c5e8a290b0fa0ac754d6787bbe2a67d33feb4
Ruby Kim <rubyruvy@gmail.com>
2022-11-30 01:07:27 -0500

    refined design, added responsive breakpoints

--- frontend/src/components/BlogPost.jsx
+++ frontend/src/components/BlogPost.jsx
@@ -2,14 +2,19 @@ import { Link } from 'react-router-dom';
 
 const Post = () => {
     return ( 
-        <div>
-            {/* <div className='text-3xl'>Title</div> */}
-            <input type="text" placeholder="Title" className="input w-full mt-20 text-xl border-primary border-y-2 border-opacity-10" />
-            <textarea className="textarea textarea-bordered border-primary border-y-2 border-opacity-10
-                                mt-4 w-full h-96 text-lg"
-                placeholder="Type here" />
-            <button className="flex btn bg-accent rounded-full text-primary ml-2 my-2 px-6 text-lg">Post</button>
+        <div className='relative w-screen h-screen bg-neutral'>
+            <div className='absolute grid grid-rows-8 gap-y-4 px-5 left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2
+                            w-screen max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-4xl'>
+                <h1 className='row-span-2 ml-1 text-2xl text-primary font-medium pb-8'> <span className='text-4xl font-bold'>ekko </span>  your voice.</h1>
+                <input type="text" placeholder="Title" className="row-span-1 input w-full text-xl rounded-2xl"/>
+                <textarea className="row-span-4 textarea rounded-2xl
+                                     mt-4 w-full h-72 text-lg
+                                     md:h-80 lg:h-96"
+                    placeholder="Type here" />
+                <button className="flex row-span-1 btn bg-accent rounded-full text-primary ml-2 my-2 px-6 text-lg">Post</button>
+            </div>
         </div>
+        
     )
 }
 
