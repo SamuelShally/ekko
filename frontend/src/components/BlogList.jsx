433aa8a187171478019984f3f5762d7241b1e0ab
Ruby Kim <rubyruvy@gmail.com>
2022-11-30 17:51:01 -0500

    workingon blog post to be able to fetch data

---
+++ frontend/src/components/BlogList.jsx
@@ -0,0 +1,40 @@
+// import Blog from "./Blog";
+import { useState } from 'react';
+
+const Bloglist = (props) => {
+    const blogs = props.blogs;
+    
+    return ( 
+        <section>
+            {blogs.map((blog) => (
+                // <Blog title={blog.title} body={blog.body} author={blog.author} date={blog.date} key={blog.id} />
+                <div className='mt-4 mx-4 rounded-xl shadow-lg overflow-hidden bg-white'
+                 key={blog.id}> 
+                <div>
+                        <img className='h-48 w-full object-cover' src={blog.img} />
+                </div>
+                <div className='p-4'>
+                    <p className='text-secondary sm:text-lg'>Blog</p>
+                    <h3 className='mt-2 text-2xl sm:text-3xl font-semibold leading-7 text-gray-900'> { blog.title } </h3>
+                    <p className='mt-2 leading-6 sm:text-lg text-gray-400 font-light'>
+                                {blog.body}
+                    </p>
+                    <div className='mt-6 flex items-center'> {/* author-1 info container */}
+                        <div className='flex-shrink-0'>
+                            <img className="h-12 w-12 rounded-full mask-circle" src="https://placeimg.com/160/160/arch" />
+                        </div>
+                        <div className='ml-4 align-middle '>
+                            <p className='text-sm sm:text-lg leading-5 font-medium text-primary'>{ blog.author }</p>
+                            <time dateTime='2022-11-15' className='text-sm sm:text-lg leading-5 text-primary opacity-50'>
+                                    { blog.date }
+                            </time>
+                        </div>        
+                    </div>
+                </div> 
+            </div>
+                ))}
+        </section>
+     );
+}
+ 
+export default Bloglist;
--- frontend/src/components/UserSpace.jsx
+++ frontend/src/components/UserSpace.jsx
@@ -1,14 +1,52 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import UserNav from './UserNav';
 import Bloglist from './BlogList';
 
 const UserSpace = () => {
     const [blogs, setBlogs] = useState([
-        { title: "8 facts about gender equality you must know", body: 'Lorem ipsum dolor sit amet. Eos repellendus unde sit omnis accusantium.', author: 'Lemon', date: 'Nov 15, 2022', id: 1 },
-        { title: "5 Ways Abortion Rights Protect Women's Health", body: 'Lorem ipsum dolor sit amet. Eos repellendus unde sit omnis accusantium.', author: 'Ruby', date: 'Nov 30, 2022', id: 2}
+        // temporary data: must be fetched later
+        {
+            title: "8 facts about gender equality you must know",
+            body: 'Lorem ipsum dolor sit amet. Eos repellendus unde sit omnis accusantium.',
+            author: 'Lemon',
+            date: 'Nov 15, 2022',
+            img: 'https://images.pexels.com/photos/6185320/pexels-photo-6185320.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+            id: 1
+        },
+        {
+            title: "5 Ways Abortion Rights Protect Women's Health",
+            body: 'Lorem ipsum dolor sit amet. Eos repellendus unde sit omnis accusantium.',
+            author: 'Ruby',
+            date: 'Nov 30, 2022',
+            img: 'https://placeimg.com/160/160/arch',
+            id: 2
+        },
+        {
+            title: "5 Ways Abortion Rights Protect Women's Health",
+            body: 'Lorem ipsum dolor sit amet. Eos repellendus unde sit omnis accusantium.',
+            author: 'Ruby',
+            date: 'Nov 30, 2022',
+            img: 'https://images.pexels.com/photos/6185320/pexels-photo-6185320.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+            id: 3
+        }
     ]);
-    
+
+    // display loading msg while fetching data
+    const [isPending, setIsPending] = useState(true);
+
+    // *** NEEDS MODIFICATION
+    // useEffect(() => {
+    //     fetch('')
+    //         .then(res => {
+    //             return (res)
+    //         })
+    //         .then(data => {
+    //             setBlogs(data);
+    //             setIsPending(false);
+    //         });
+    // }, []);
+
     return (
         <div className="min-h-screen h-full bg-white px-4 mt-4 md:mt-6 lg:px-10">
             <UserNav/>
@@ -16,7 +54,7 @@ const UserSpace = () => {
             {/* <h2 className='mt-4 text-center tracking-notmal'>My Profile</h2> */}
             <div className='flex flex-col mt-10 md:mt-14 
                             w-full text-center items-center justify-center'>
-                <img className="relative w-30 h-30 rounded-full justify-self-center" src="https://placeimg.com/160/160/arch" />
+                <img className="relative w-30 h-30 rounded-full justify-self-center" src='https://placeimg.com/160/160/arc' />
                 <h4 className='mt-2 text-gray-500 sm:text-xl md:text-xl'>@username</h4>
             </div>
 
@@ -30,13 +68,14 @@ const UserSpace = () => {
                 <div className="grid gap-5 max-w-lg mx-auto py-6 bg-neutral rounded-lg 
                         lg:grid-cols-3 lg:max-w-none ">
                     
-                    <Bloglist blogs={blogs} /> 
-                
-                    
-                      
+                    {/* {isPending && <div>Loading...</div>} */}
+
+                    {/* render it once we have the data & show corresponding user's blog only */}
+                    {/* *** NEEDS MODIFICATION */}
+                    {blogs && <Bloglist blogs={blogs.filter((blog) => blog.author=='Ruby')} />}  
+                  
                 </div>  
             </div>
-            {/* <div className='w-full h-96'></div> */}
         </div>
     )
 }
