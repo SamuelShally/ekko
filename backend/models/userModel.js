234a9f92760a6dbb97466c1e08cc99926e134a12
Ruby Kim <rubyruvy@gmail.com>
2022-11-30 19:15:37 -0500

    trying to merge Loginform front+back

---
+++ backend/models/userModel.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+
+    {
+        username:{
+            type:String,
+            unique:true,
+            required: true
+        },
+        email: {
+            type:String,
+            unique:true,
+            required: true
+        },
+        password: {
+            type:String,
+            required: true
+            
+        },
+        worldview:{
+            type:String
+        },
+        interests:{
+            type:[String]
+        },
+        intro:{
+            type:String
+        }
+    }
+);
+
+module.exports = mongoose.model('User',userSchema); //collection
--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,15 @@ require('dotenv').config(); //attach env variables to the process object
 const express = require('express');
 const cors = require('cors')
 const mongoose = require('mongoose');
-
+const userRoutes = require("./routes/users");
 
 //register the express app
 const app = express();
 
 //middleware
-app.use(cors())
+app.use(cors()); //request from any dom.
 app.use(express.json()); //getting access to req.body
+app.use('/api/users', userRoutes);
 
 //show req.path and req.mothod of the request.
 app.use((req,res,next)=>{
@@ -19,13 +20,6 @@ app.use((req,res,next)=>{
     next();
 })
 
-const router = express.Router();
-
-router.get("/hi", (req,res)=>{
-    res.status(200).json({msg:"Ekko"});
-})
-
-app.use('/', router)
 
 //connecting to mongo db
 mongoose.connect(process.env.MONGO_URI)
@@ -37,4 +31,90 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((error)=>{
         console.log("Bad news! Something went wrong");
         console.log(error);
+})
+
+
+
+
+
+
+/* **********SESSIONS FRAMEWORK********* */
+//All changes to Server.js are below this line
+//Please do NPM install. New packages were used.
+
+/*
+    How this works (I think): 
+    
+    We will set a cookie on the front end that will remember 
+    if we're logged in or not - or whatever else we want it to remember.
+
+    This cookie will then compare against an identical copy saved to our mongoDB 
+    database. If it exists and isAuth = true, we can be logged in as that user
+*/
+
+/*
+const session = require('express-session');
+const MongoDBSession = require('connect-mongodb-session')(session);
+const User = require('./models/userModel');
+
+//Database for saving the user session
+const store = new MongoDBSession({
+    uri: process.env.MONGO_URI,
+    collection: 'mySessions',
+})
+
+*/
+
+
+//check if user is logged in, if not: redirect to log in page
+
+
+//const isAuth = (req, res, next) => {
+    //if(req.session.isAuth){
+     //   next()
+   // }else{
+        // TODO: Redirect the user back to log in page
+
+          /*
+            Remark: I don't know where to redirect to right now. 
+            I need to understand the combination between front and backend better
+        */
+
+     //  next()
+  //  }
+//}
+
+
+//Set up the session cookie to req objects
+
+/*
+app.use(
+    
+    session({
+        secret: 'test_user_key', //Str that will sign the cookie 
+        resave: false, //New session for every request t/f
+        saveUninitialized: false, //save if session is unmodified t/f 
+        store: store, //Save session to Database
+        cookie: {
+            sameSite: "none",
+            secure: false, 
+            maxAge: 100*60*60,
+        }
     })
+
+);
+
+
+
+
+
+// For backend sessions testing  
+app.get("/", (req, res) => {
+    //Temp for allowing access
+    // req.session.isAuth = true; 
+    res.send("Testing sessions");
+});
+
+*/
+
+
