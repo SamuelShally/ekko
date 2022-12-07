c4f1b9eec95f4b3c8bc12a9d4b66bd6f947c4989
Ruby Kim <rubyruvy@gmail.com>
2022-12-07 00:05:27 -0500

    carret color

--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -48,7 +48,7 @@ const RegisterForm = () => {
                             <label className="label"/>
                         <input type="text" placeholder="Username"
                                 className="input w-full rounded-full border-solid border-2
-                                input-md font-light text-xs"
+                                input-md font-light text-xs caret-secondary"
                             value={username}
                             onChange={(e)=>setUsername(e.target.value)}
                                 />
@@ -57,7 +57,7 @@ const RegisterForm = () => {
                             <label className="label"/>
                         <input type="text" placeholder="Email address"
                                 className="input w-full rounded-full border-solid border-2
-                                input-md font-light text-xs"
+                                input-md font-light text-xs caret-secondary"
                                 value={email}
                             onChange={(e)=>setEmail(e.target.value)}
                                 />
@@ -66,7 +66,7 @@ const RegisterForm = () => {
                             <label className="label"/>
                             <input type="password" placeholder="Set password"
                                 className="input w-full rounded-full border-solid border-2
-                                input-md text-xs"
+                                input-md text-xs caret-secondary"
                                 value={password}
                                 onChange={(e)=>setPassword(e.target.value)}
                                 />
@@ -81,7 +81,7 @@ const RegisterForm = () => {
                         </button>
                         
                         {error && 
-                            <div className="alert shadow-lg bg-secondary text-neutral rounded-full  -mt-2 p-4 text-center justify-center max-w-xs">
+                            <div className="alert shadow-lg bg-secondary text-neutral rounded-lg -mt-2 p-4 text-center justify-center max-w-xs">
                                 <div className='text-center'>
                                     {/* <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg> */}
                                     <span>{error}</span>
