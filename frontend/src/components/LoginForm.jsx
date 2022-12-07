ad7851b78491360d81493f92e70f2e62e3582ba4
Ruby Kim <rubyruvy@gmail.com>
2022-12-06 20:16:10 -0500

    form style adjustment

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -38,46 +38,54 @@ const LoginForm = () => {
                     </Link>
                 </div>
 
-                <div className="relative grid gap-y-14 place-content-center h-screen z-0
+                <div className="relative grid gap-y-14 place-content-center h-screen z-0 
                                 min-w-min sm:col-span-2">
-                    <div className="h-0 place-self-end">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="w-20 h-16 scale-75 justify-self-end -mr-4" fill="none"><path fill="#DEDBC0" d="M35.786 35.009c14.869-7.167-20.51 1.162 3.099-32.858C56.669.955 66.606 13.566 67.84 30.798c1.234 17.23-9.178 32.354-24.112 32.354-6.86-11.568-17.532-21.331-7.942-28.143Z"/><path fill="#29180F" d="M49.715 29.833c-.426 2.318-2.25 4.048-3.969 5.657-6.642 6.218-11.253 13.861-1.413 27.662-21.517 1.447-40.156-10.76-41.39-27.99C1.707 17.93 18.577 3.597 40.094 2.15c-14.341 10.19 11.774 15.96 9.62 27.682Z"/></svg>
-                    </div>
-                    <h1 className="text-3xl font-semibold">Welcome back</h1>
+                    <div className='grid gap-y-8 border-2 rounded-xl shadow-xl mt-16 p-10 mx-10 sm:mt-0'>
+                        <div>
+                            <div className="h-0 place-self-end ml-64 mb-20">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="w-20 h-16 scale-75 justify-self-end -mr-4" fill="none"><path fill="#DEDBC0" d="M35.786 35.009c14.869-7.167-20.51 1.162 3.099-32.858C56.669.955 66.606 13.566 67.84 30.798c1.234 17.23-9.178 32.354-24.112 32.354-6.86-11.568-17.532-21.331-7.942-28.143Z"/><path fill="#29180F" d="M49.715 29.833c-.426 2.318-2.25 4.048-3.969 5.657-6.642 6.218-11.253 13.861-1.413 27.662-21.517 1.447-40.156-10.76-41.39-27.99C1.707 17.93 18.577 3.597 40.094 2.15c-14.341 10.19 11.774 15.96 9.62 27.682Z"/></svg>
+                            </div>
+                            <h1 className=" font-semibold text-3xl lg:text-4xl">Welcome back</h1>
+                        </div>
                     <form className="grid gap-6">
-                        <div className="form-control w-full max-w-xs">
+                        <div className="form-control w-full">
                             <label className="label"/>
 
                         <input type="text" placeholder="Username"
-                                className="input w-80 max-w-xs rounded-full border-solid border-2
-                                input-md font-light text-xs"
+                                className="input w-80 rounded-full border-solid border-2
+                                input-md lg:input-lg
+                                font-light text-xs shadow-inner"
                                 value={username}
                                 onChange = {(e)=>setUsername(e.target.value)}
                                 />
-
                         </div>
-                        <div className="form-control w-full max-w-xs">
+                        <div className="form-control w-full ">
                             <label className="label"/>
                             <input type="password" placeholder="Password"
-                                className="input w-full max-w-xs rounded-full border-solid border-2
-                                input-md font-light text-xs"
+                                className="input w-full rounded-full border-solid border-2
+                                input-md lg:input-lg  
+                                font-light text-xs shadow-inner"
                                 value={password}
                                 onChange = {(e)=>setPassword(e.target.value)}/>
                         </div>
+                        <Link to="/">
                         <button
                             className="text-lg text-neutral rounded-full bg-primary
-                            px-20 w-80 btn-md"
+                            px-20 w-80 btn-md hover:bg-neutral hover:text-primary hover:shadow-inner"
                             onClick = {submitForm}>
                                 Sign in
-                        </button>
+                            </button>
+                        </Link>
                     </form>
                     {error && 
-                            <div className="alert shadow-lg bg-secondary text-neutral rounded-full -mt-8 p-4 text-center justify-center max-w-xs">
+                            <div className="alert shadow-lg bg-secondary text-neutral rounded-lg -mt-2 p-4 text-center justify-center max-w-s">
                             <div className='text-center'>
-                                {/* <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg> */}
+                                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
                                 <span>{error}</span>
                             </div>
-                        </div>                    }
+                        </div>
+                        }
+                    </div>
                 </div>
 
         
