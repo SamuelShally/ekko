81c8588cac8e340a1f59bd1b9ace20110b2cc63b
CharlotteHuliyi <CharlotteHuliyi@gmail.com>
2022-12-10 16:42:22 +0800

    fix error

--- frontend/src/components/SimilarUsers.jsx
+++ frontend/src/components/SimilarUsers.jsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 // left & right margins: 6
-import { useEffect, useState } from "react";
 import { useAuthContext } from "../hooks/useAuthContext";
 import UserDetails from './UserDetails';
 
